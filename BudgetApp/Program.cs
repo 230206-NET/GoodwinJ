d2d4920f72b5c58382954958d42f84c646e912a1
jacob <jacob.carter.goodwin@gmail.com>
2023-02-09 17:08:28 -0700

    Jacob and Samuel Hangman Game

--- BudgetApp/Program.cs
+++ BudgetApp/Program.cs
@@ -1,16 +1,22 @@
-﻿Console.WriteLine("Budget App to track expenses");
+﻿//Title of app
+Console.WriteLine("Budget App to track expenses");
 
+//Get initial budget from user
 Console.WriteLine("Enter your initial budget:");
 int budget = int.Parse(Console.ReadLine()!);
 int remainder = budget;
 
+//Get number of expenses from user
 Console.WriteLine("How many expenses do you have?");
 int numExpenses = int.Parse(Console.ReadLine()!);
 
+//set up expense array and counter
 Task[] expenseArr = new Task[numExpenses];
 int expenseCounter = 0;
 
 while(true) {
+    
+    //Get expense description from user
     Console.WriteLine("Input Expense Description:");
     string inputExpense = Console.ReadLine()!;
     Task expenseToCreate = new Task();
@@ -18,12 +24,17 @@ while(true) {
     if(expenseCounter == expenseArr.Length) {
         expenseArr = ResizeArray(expenseArr);
     }
+
+    //Get expense amount from user
     Console.WriteLine("Input Expense Amount");
     expenseToCreate.Amount = Console.ReadLine()!;
 
+    //populate array with expense description and amount
     expenseArr[expenseCounter] = expenseToCreate;
 
     expenseCounter++;
+
+    //ask user if there are more expenses to add
     Console.WriteLine("Add another expense?");
     string response = Console.ReadLine()!;
     if(response == "n") {
@@ -31,6 +42,7 @@ while(true) {
     }
 }
 
+//Method to extend array if necessary
 Task[] ResizeArray(Task[] arrToResize) {
     Task[] newArr = new Task[arrToResize.Length * 2];
     for(int i = 0; i < arrToResize.Length; i++) {
@@ -39,10 +51,12 @@ Task[] ResizeArray(Task[] arrToResize) {
     return newArr;
 }
 
+//Print all expense descriptions and their amounts
 for(int i = 0; i < expenseArr.Length; i++) {
     Console.WriteLine("Description: {0}, Amount: {1}", expenseArr[i].Description, expenseArr[i].Amount);
 }
 
+//Print the remaining budget
 for (int i = 0; i < expenseArr.Length; i++){
     int price = int.Parse(expenseArr[i].Amount);
     remainder = remainder - price;
---
+++ Hangman/Program.cs
@@ -0,0 +1,110 @@
+﻿// # 230209 OOP C#
+
+// ## Exercise: Hangman Game
+// Practice string manipulation and OOP by making a hangman game console application
+
+// - It should present the user with a word to guess (either random, or hard code)
+// - Allow users to select/enter a letter
+// - It should display number of letters as blank spaces
+// - If the user guesses the letter correctly, it should fill in those blank spaces
+// - It should draw the hangman, or at the very least have a limited number of tries
+// - It should validate the user input that it is an alphabet (and not a special character, whitespace, number, etc.)
+// - If the user guesses the word correctly, it should end the game ("you got it, congrats!")
+// - If the user makes too many incorrect guesses, it also should end the game ("whomp whomp.. perhaps next time")
+
+// 8: Jacob Goodwin, Samuel Perrino
+
+Console.WriteLine("Hangman");
+
+string chars = "stove";
+char[] word = {'_', '_', '_', '_', '_'};  
+
+string word1 = new string(word);
+
+Console.WriteLine("Word:");
+Console.WriteLine(word1);
+
+bool end = true;
+int counter = 0;
+int integer;
+
+guessAgain:
+
+if(word1.CompareTo("stove")==0){
+    Console.WriteLine("Congratulations!");
+    end = false;
+    }
+
+if(counter == 6){
+    Console.WriteLine("You lose");
+    end = false;
+}
+
+string guess = " ";
+
+if(end) {
+Console.WriteLine("Guess a letter");
+
+ guess = Console.ReadLine()!;
+}
+
+bool parseSuccess = int.TryParse(guess, out integer);
+
+if(end) {
+if(string.IsNullOrWhiteSpace(guess)){
+    Console.WriteLine("input value must not be empty");
+    goto guessAgain;
+} else if (parseSuccess){
+    Console.WriteLine("input value must not be an integer");
+    goto guessAgain;
+}
+}
+while(end){
+for (int i =0; i<chars.Length;i++){
+
+       
+
+    if(guess.CompareTo("s")==0)
+     {
+         word[0] = 's';
+         word1 = new string(word);
+         Console.WriteLine(word1);
+        goto guessAgain;
+     }
+     else if(guess.CompareTo("t")==0)
+     {
+         word[1] = 't';
+         word1 = new string(word);
+         Console.WriteLine(word1);
+         goto guessAgain;
+     }
+     else if(guess.CompareTo("o")==0)
+     {
+         word[2] = 'o';
+         word1 = new string(word);
+         Console.WriteLine(word1);
+         goto guessAgain;
+     }
+     else if(guess.CompareTo("v")==0)
+     {
+         word[3] = 'v';
+         word1 = new string(word);
+         Console.WriteLine(word1);
+         goto guessAgain;
+     }
+     else if(guess.CompareTo("e")==0)
+     {
+         word[4] = 'e';
+         word1 = new string(word);
+         Console.WriteLine(word1);
+         goto guessAgain;
+     } else {
+        counter++;
+        goto guessAgain;
+     }
+     
+}
+}
+
+
+
